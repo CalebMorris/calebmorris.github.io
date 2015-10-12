75dffb0d519114d0479e7622e92d5b4209f26ed1
Caleb Morris <caleb.morris.g@gmail.com>
2015-10-11 20:29:32 -0700

    Added problem workthrough
    Plan to add more and create a blog exposition of problems

---
+++ blergs/code/3-11/LookupTable/LookupTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Test
+{
+	public class LookupTable {
+		private int[,] m_table;
+		private int m_size;
+
+		private int shouldValueMore(int overrideValue, int nextValue) {
+			if (overrideValue <= nextValue) {
+				// Prefer older value
+				return overrideValue;
+			}
+
+			return nextValue;
+		}
+
+		public LookupTable(int[] values) {
+			m_size = values.Length;
+			m_table = new int[m_size, m_size];
+			for(int i = 0; i < m_size; i++) {
+				for(int j = i; j < m_size; j++) {
+					int min = values[i];
+					for(int current = i + 1; current <= j; current++) {
+						min = shouldValueMore (min, values [current]);
+					}
+					m_table[i, j] = min;
+				}
+			}
+		}
+
+		public int Lookup(uint i, uint j) {
+			if (i > m_size - 1 || j > m_size - 1) {
+				throw new InvalidOperationException("Invalid size: (" + i + "," + j + ")");
+			}
+			if (j < i) {
+				throw new InvalidOperationException("Upper point cannot be smaller than lower bound");
+			}
+
+			return m_table[i, j];
+		}
+	}
+}
+
---
+++ blergs/code/3-11/LookupTable/LookupTableTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test
+{
+	[TestFixture]
+	public class LookupTableTests
+	{
+		[Test]
+		public void InvalidInputs() {
+			LookupTable t = new LookupTable (new int[] { 4, 2, 1, 3 });
+			Assert.Throws<InvalidOperationException> (delegate {
+				t.Lookup(1, 200);
+			});
+		}
+
+		[Test]
+		public void SimpleTest() {
+			LookupTable t = new LookupTable (new int[] { 4, 2, 1, 3 });
+			Assert.AreEqual (4, t.Lookup (0, 0));
+		}
+
+		[Test]
+		public void LongTest() {
+			LookupTable t = new LookupTable (new int[] { 4, 2, 1, 3 });
+			Assert.AreEqual (1, t.Lookup (0, 3));
+		}
+
+		[Test]
+		public void NegativeTest() {
+			LookupTable t = new LookupTable (new int[] { -4, -2, -1, -3 });
+			Assert.AreEqual (-4, t.Lookup (0, 3));
+		}
+
+		[Test]
+		public void MixedSignTest() {
+			LookupTable t = new LookupTable (new int[] { 4, -2, 1, -3 });
+			Assert.AreEqual (-3, t.Lookup (0, 3));
+		}
+
+		[Test]
+		public void EdgeTest() {
+			LookupTable t = new LookupTable (new int[] { 2, 3, 1 });
+			Assert.AreEqual (1, t.Lookup (0, 2));
+
+			LookupTable t2 = new LookupTable (new int[] { 1, 2, 4 });
+			Assert.AreEqual (1, t2.Lookup (0, 2));
+		}
+	}
+}
+

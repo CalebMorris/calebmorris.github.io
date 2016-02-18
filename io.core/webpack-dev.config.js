c6e88a442de0461f855f1e6463d044798f3eab01
Caleb Morris <caleb.morris.g@gmail.com>
2016-02-17 20:47:19 -0700

    Updated scripts to use relative base

--- io.core/webpack-dev.config.js
+++ io.core/webpack-dev.config.js
@@ -12,7 +12,7 @@ var devLoaders = [
 module.exports = {
   entry: [
     // setup the hot mobule loading
-    'webpack-dev-server/client?http://localhost:8080',
+    'webpack-dev-server/client?http://localhost:8080/',
     'webpack/hot/only-dev-server',
     // our entry file
     './app/main.jsx'
@@ -26,7 +26,7 @@ module.exports = {
     // proxy calls to api to our own node server backend
     proxy: {
       '/api/*': 'http://localhost:5000/'
-    }
+    },
   },
   module: {
     loaders: commonConfig.loaders.concat(devLoaders)
